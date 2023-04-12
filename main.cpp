944c8cf3694b035aca62c799c7a1ecd0e9d4624f
Mo2Hefny <moamensaied1@gmail.com>
2023-04-13 00:33:38 +0200

    test

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include "Scheduler/Scheduler.h"
 
+
+
 int main()
 {
-	Scheduler* schedule_manager = new Scheduler;
-	
+	//Scheduler* schedule_manager = new Scheduler;
+	CircularList<int> c;
+	int n = 0;
+	while (n != -1)
+	{
+		cin >> n;
+		c.InsertEnd(n);
+	}
+	c.PrintList();
+	c.pop();
+	c.pop();
+	c.PrintList();
 
-	if (schedule_manager)
-		delete schedule_manager;
+	//if (schedule_manager)
+		//delete schedule_manager;
 	return 0;
 }
